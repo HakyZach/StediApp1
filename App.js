4c174f6037bfe9e365684269cd3725adf984e62c
Zachary Scieneaux <zscieneaux@sbcglobal.net>
2022-11-03 14:32:41 -0600

    login

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState, } from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity, AsyncStorage, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, TextInput, Button, Alert } from 'react-native';
 import  Navigation from './components/Navigation';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import OnboardingScreen from './screens/OnboardingScreen';
 import Home from './screens/Home';
 import { NavigationContainer } from '@react-navigation/native';
-
+import {DrawerContentScrollView} from '@react-navigation/drawer';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 
@@ -13,10 +14,28 @@ const AppStack = createNativeStackNavigator();
 
 const App = () =>{
   const [isFirstLaunch, setFirstLaunch] = React.useState(true);
+  const [phoneNumber, setPhoneNumber] =React.useState("");
   const [isLoggedIn,setIsLoggedIn] = React.useState(false);
   const [homeTodayScore, setHomeTodayScore] = React.useState(0);
+  const [tempCode, setTempCode] = React.useState(null);
 
-   if (isFirstLaunch == true){
+  useEffect(()=>{
+    const getSessionToken = async()=> {
+      const sessionToken = await AsyncStorage.getItem('sessionToken');
+      console.log('token from storage', sessionToken);
+
+      const validateResponse = await fetch('https://dev.stedi.me/validate/'+sessionToken);
+      
+      if(validateResponse.status == 200){
+        const userEmail = await validateResponse.text();
+        console.log('userEmail', userEmail);
+        setIsLoggedIn(true);
+      }
+    }
+    getSessionToken();
+  },[])
+
+   if (isFirstLaunch == true &&! isLoggedIn){
 return(
   <OnboardingScreen setFirstLaunch={setFirstLaunch}/>
  
@@ -27,16 +46,72 @@ return(
     return (
       <View>
         <TextInput
-        style={StyleSheet.input}
+        value={phoneNumber}
+        onChangeText={setPhoneNumber}
+        style={styles.input}
         placeholderTextColor='#4251f5'
         placeholder='Cell Phone'>
         </TextInput>
         <Button
         title='Send'
         style={styles.button}
-        onPress={()=>{
-          console.log('Button was pressed')
+        onPress={async()=>{
+          console.log(phoneNumber + 'Button was pressed')
+
+          await fetch(
+            'https://dev.stedi.me/twofactorlogin/'+ phoneNumber,
+            {
+              method: 'POST',
+              headers:{
+                'content-type' : 'application/text'
+              }
+              
+            }
+          )
         }}
+      />
+
+<TextInput
+        value={tempCode}
+        onChangeText={setTempCode}
+        style={styles.input2}
+        placeholderTextColor='#4251f5'
+        placeholder='Enter Code'>
+        </TextInput>
+        <Button
+        title='Verify'
+        style={styles.button}
+        onPress={async()=>{
+          console.log('Button 2 was pressed')
+
+          const loginResponse=await fetch(
+            "https://dev.stedi.me/twofactorlogin",
+            {
+              method: "POST",
+              headers:{
+                "content-type" : "application/text"
+              },
+              body:JSON.stringify({
+                phoneNumber,
+                oneTimePassword:tempCode
+              })
+            }
+          )
+          console.log("status",loginResponse.status)
+          
+
+          if(loginResponse.status == 200){
+            const sessionToken = await loginResponse.text();
+            await AsyncStorage.setItem('sessionToken',sessionToken)
+            console.log('Session Token', sessionToken);
+
+            setIsLoggedIn(true);
+          }
+          else{
+            console.log('token response Status', loginResponse.status)
+            Alert.alert('Warning', 'An invalid code was entered.')
+          }
+        }}   
       />
       </View>
     )
@@ -55,7 +130,14 @@ return(
     margin: 12,
     borderWidth: 1,
     padding: 10,
-    marginTop: 350
+    marginTop: 250
+  },
+  input2: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    marginTop: 12
   },
   margin:{
     marginTop:100
@@ -66,3 +148,4 @@ return(
     padding: 10
   }    
 })
+
